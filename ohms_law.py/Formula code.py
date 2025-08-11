abe622116fbf719614b5b289813c7e300dda90ba
Bossyala <simbamanzwiyi99@gmail.com>
2025-08-11 22:15:11 +0300

    Ohms law

---
+++ ohms_law.py/Formula code.py
@@ -0,0 +1,8 @@
+# Ohms Law V = I * R
+# V is the voltage in volts, I is the current in amperes and R is the resistance in ohms
+I = float(input("\nEnter the Current in Amps, (A):  "))
+R = float(input("\nEnter the resistance in Ohms: "))
+
+V = float(I * R)
+
+print("\nThe voltage, V, is : ", V,"V\n")
